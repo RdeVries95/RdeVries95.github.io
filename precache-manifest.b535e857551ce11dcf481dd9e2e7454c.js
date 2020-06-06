34514ab3fd7f9218d2e896ad09ce485067cddd33
Ricardo de Vries <ricardo@MacBook-Pro-van-Ricardo.local>
2020-06-06 21:57:39 +0200

    Updates

---
+++ precache-manifest.b535e857551ce11dcf481dd9e2e7454c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e2bee9edb633355ae53fd7bf2c7515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "243c17e65f55d99e90fb",
+    "url": "/static/css/main.a26ecbe4.chunk.css"
+  },
+  {
+    "revision": "46ffbac1f8c6b47dca5f",
+    "url": "/static/js/2.00a16e7c.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.00a16e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243c17e65f55d99e90fb",
+    "url": "/static/js/main.17cef823.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  },
+  {
+    "revision": "5c345b90c45c6fad10386d96cc93a030",
+    "url": "/static/media/Starjedi.5c345b90.woff"
+  },
+  {
+    "revision": "67a9eb0c2a02c77195f82ba766701b4a",
+    "url": "/static/media/about-image.67a9eb0c.jpg"
+  },
+  {
+    "revision": "8697d933d16b806d74da2b772ce15ea0",
+    "url": "/static/media/cocktail-image.8697d933.gif"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/static/media/css3-logo.3707da3c.svg"
+  },
+  {
+    "revision": "525f317fe9e90927edf7694d88ce42c8",
+    "url": "/static/media/html5-logo.525f317f.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/static/media/js-logo.87711407.svg"
+  },
+  {
+    "revision": "f972a03faeab6d96d6b737ec16aa3efa",
+    "url": "/static/media/movie-image.f972a03f.gif"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/reactjs-logo.05a3e30f.svg"
+  },
+  {
+    "revision": "e0f651ac7aecf9187f66e5bd5f181c10",
+    "url": "/static/media/sass-logo.e0f651ac.svg"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/static/media/styled-components-logo.e79a42b9.svg"
+  },
+  {
+    "revision": "49114955510f48977cf7c147f0a1562d",
+    "url": "/static/media/waving.49114955.gif"
+  },
+  {
+    "revision": "d46c897e979f2cc6b9764bee26c665a6",
+    "url": "/static/media/yoda-image.d46c897e.gif"
+  }
+]);
